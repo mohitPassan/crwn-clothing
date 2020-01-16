536c546f4a911593dd974f7e827cfc4b85a22b9e
Mohit Passan <mohitpassan@gmail.com>
2020-01-16 22:14:38 +0530

    Styles and transitions to menu items

--- src/components/menu-items/menu-items.components.jsx
+++ src/components/menu-items/menu-items.components.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import './menu-items.styles.scss'
 
 const MenuItem = (props) => (
-    <div style = {{
-        backgroundImage: `url(${props.imageUrl})`
-    }} className={`${props.size} menu-item`}>
+    <div className={`${props.size} menu-item`}>
+        <div className="backgroundImg" style = {{
+            backgroundImage: `url(${props.imageUrl})`
+        }} />
         <div className="content">
             <h1 className="title">
-                {props.title}
+                {props.title.toUpperCase()}
             </h1>
             <span className="subtitle">
                 SHOP NOW
